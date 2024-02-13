d23e683b8a43410697edf4110d2e7e9f4d6145f0
shailesh3010 <shailesh.pandit3010@gmail.com>
2024-02-13 17:14:28 +0530

    second commit

--- src/main/java/AmazoneImplementation/Search.java
+++ src/main/java/AmazoneImplementation/Search.java
@@ -2,11 +2,17 @@ package AmazoneImplementation;
 
 public class Search {
 	
+	
+	
 	public String displayProduct(Product product) {
 		if(product.getProductList().contains(product.getProductName())) {
 			return product.getProductName();
 		}
 		return null;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("new comment");
 	}
 
 }
